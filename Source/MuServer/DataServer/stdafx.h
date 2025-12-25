63ae146b0c7914731990c7b5381158c6803a3b02
Olddies <xinoperesa@gmail.com>
2025-12-26 00:26:26 +0100

    Add missing POSIX WinAPI typedefs

--- Source/MuServer/DataServer/stdafx.h
+++ Source/MuServer/DataServer/stdafx.h
@@ -1,13 +1,7 @@
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN
-
-#define _WIN32_WINNT _WIN32_WINNT_VISTA
-
 // System includes
-#include <windows.h>
-#include <dbghelp.h>
-#include <winsock2.h>
+#include "../Platform/Platform.h"
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -16,15 +10,12 @@
 #include <time.h>
 #include <sql.h>
 #include <sqlext.h>
+#ifdef _WIN32
 #include <mbstring.h>
-#include <WS2tcpip.h>
+#endif
 #include <algorithm>
 #include <vector>
 #include <fstream>
 
 // General includes
 #include "Console.h"
-
-// Libraries
-#pragma comment(lib,"dbghelp.lib")
-#pragma comment(lib,"ws2_32.lib")
